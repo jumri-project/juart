Correct rotation

src/juart/ellipsoid_phantoms/ellipsoids.py
@@ -171,7 +171,7 @@ class Geometry:
         # Transform ellipsoid back to unit sphere
         ellip_shift = r - self.center[:, None]
 
-        ellip_rot = torch.matmul(self.rot_matrix, ellip_shift)
+        ellip_rot = torch.matmul(self.rot_matrix.T, ellip_shift)
 
         sphere = ellip_rot**2 / self.axes[:, None] ** 2
 
@@ -877,7 +877,7 @@ Ellipsoids_3D = pd.DataFrame({
     'axis_a':       [0.720  , 0.69  , 0.6624    , 0.6524    , 0.41  , 0.31  , 0.210     , 0.046     , 0.046     , 0.046     , 0.046     , 0.023     , 0.056     , 0.056  , 0.2       ], # noqa: E501
     'axis_b':       [0.95   , 0.92  , 0.874     , 0.864     , 0.16  , 0.11  , 0.25      , 0.046     , 0.023     , 0.023     , 0.046     , 0.023     , 0.04      , 0.056  , 0.03      ], # noqa: E501
     'axis_c':       [0.93   , 0.9   , 0.88      , 0.87      , 0.21  , 0.22  , 0.35      , 0.046     , 0.02      , 0.02      , 0.046     , 0.023     , 0.1       , 0.1    , 0.1       ], # noqa: E501
-    'angle':        [0.0    , 0.0   , 0.0       , 0.0       , 72.0 , -72.0  , 0.0       , 0.0       , 0.0       , -90.0     , 0.0       , 0.0       , -90.0     , 0.0    , 70.0      ], # noqa: E501
+    'angle':        [0.0    , 0.0   , 0.0       , 0.0       , -72.0 , 72.0  , 0.0       , 0.0       , 0.0       , -90.0     , 0.0       , 0.0       , -90.0     , 0.0    , 70.0      ], # noqa: E501
     'type':         ['scalp', 'bone','csf'      , 'gray'    , 'csf' , 'csf' , 'white'   , 'tumor'   , 'tumor'   , 'tumor'   , 'tumor'   , 'tumor'   , 'tumor'   , 'csf'  , 'clot'    ], # noqa: E501
 })
 
@@ -886,7 +886,7 @@ Ellipsoids_2D = pd.DataFrame({
     'center_y':     [0.00   , 0.00  , -0.0184   , -0.0184   , 0.000 , 0.00  , 0.35      , 0.1       , -0.605    , -0.605    , -0.10     , -0.605    ], # noqa: E501
     'axis_a':       [0.72   , 0.69  , 0.6624    , 0.6524    , 0.41  , 0.31  , 0.21      , 0.046     , 0.046     , 0.046     , 0.046     , 0.023     ], # noqa: E501
     'axis_b':       [0.95   , 0.92  , 0.874     , 0.864     , 0.16  , 0.11  , 0.25      , 0.046     , 0.023     , 0.023     , 0.046     , 0.023     ], # noqa: E501
-    'angle':        [0.0    , 0.0   , 0.0       , 0.0       , 72.0 , -72.0  , 0.0       , 0.0       , 0.0       , -90.0     , 0.0       , 0.0       ], # noqa: E501
+    'angle':        [0.0    , 0.0   , 0.0       , 0.0       , -72.0 , 72.0  , 0.0       , 0.0       , 0.0       , -90.0     , 0.0       , 0.0       ], # noqa: E501
     'type':       ['scalp', 'bone','csf'      , 'gray'    , 'csf' , 'csf' , 'white'   , 'tumor'   , 'tumor'   , 'tumor'   , 'tumor'   , 'tumor'   ], # noqa: E501
 })
 