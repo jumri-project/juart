fixed the notebook for creating numerical 3d trajectories and coilsens

src/juart/recon/ncgrappa.py
@@ -5,7 +5,7 @@ from typing import Optional, Union
 import torch
 from scipy.spatial import KDTree
 from torch.nn.functional import grid_sample
-from torch_geometric.utils import lexsort
+#from torch_geometric.utils import lexsort
 from tqdm.auto import tqdm
 
 from ..conopt.functional.fourier import (

src/juart/vis/interactive.py
@@ -1,6 +1,63 @@
 import matplotlib.pyplot as plt
 from ipywidgets import interactive, widgets
+import numpy as np
 
+class InteractiveMultiPlotter3D:
+    def __init__(
+        self,
+        data: list,
+        vmin: int = None,
+        vmax: int = None,
+        title: str = None,
+        cmap: str = "gray",
+        description = "Dimension 3:"
+    ):
+        self.data = data
+        self.vmin = vmin
+        self.vmax = vmax
+        self.title = title
+        self.cmap = cmap
+        self.description = description
+
+        self.fig, self.ax = plt.subplots(1, len(data))
+        plt.title(self.title)
+
+        i = 0
+        if type(self.ax) is np.ndarray:
+            for ax in self.ax:
+                print(ax)
+                ax.imshow = plt.imshow(
+                    self.data[i][:, :, 0], vmin=self.vmin, vmax=self.vmax, cmap=self.cmap
+                )
+                i += 1
+        else:
+            self.ax.imshow = plt.imshow(
+                self.data[i][:, :, 0], vmin=self.vmin, vmax=self.vmax, cmap=self.cmap
+            )
+
+        self.fig.colorbar(self.ax.imshow)
+
+        self.interactive = interactive(
+            self.show,
+            z=widgets.IntSlider(
+                min=1,
+                max=self.data[0].shape[2],
+                value=self.data[0].shape[2] // 2,
+                description=description,
+            ),
+        )
+
+    def show(self, z):
+
+        if type (self.ax) is np.ndarray:
+            for i, ax in enumerate(self.ax):
+    
+                ax.imshow.set_data(self.data[i][:, :, z - 1])
+                self.fig.canvas.flush_events()
+
+        else:
+            self.ax.imshow.set_data(self.data[0][:, :, z - 1])
+            self.fig.canvas.flush_events()
 
 class InteractiveFigure3D:
     def __init__(
@@ -14,6 +71,7 @@ class InteractiveFigure3D:
         cmap=None,
         description="Dimension 3",
     ):
+
         self.img = data.transpose(axes)
         self.vmin = vmin
         self.vmax = vmax
@@ -24,11 +82,11 @@ class InteractiveFigure3D:
 
         self.fig = plt.figure(figure, figsize=(3, 3))
         plt.title(self.title)
-        self.ax = self.fig.add_subplot(1, 1, 1)
+        #self.ax = self.fig.add_subplot(1, 1, 1)
         self.imshow = plt.imshow(
             self.img[:, :, 0], vmin=self.vmin, vmax=self.vmax, cmap=cmap
         )
-        self.ax.axis("off")
+        #self.ax.axis("off")
         self.fig.colorbar(self.imshow)
 
         self.interactive = interactive(