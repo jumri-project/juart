edce77dcc495d9733f1092a4175c5a1656e14d7f
timkosch <t.koschmieder@fz-juelich.de>
2025-12-04 12:46:49 +0000

    implemented ema for loss smoothing and cleaned some dl notebooks

--- src/juart/dl/model/unrollnet.py
+++ src/juart/dl/model/unrollnet.py
@@ -138,7 +138,6 @@ class UnrolledNet(nn.Module):
         self.validation_level = validation_level
 
         nX, nY, nZ, nTI, nTE = shape
-        contrasts = nTI * nTE
 
         if type(device) == list:
 
